df5583d619de85f66c3fc3c3a95a5f28d387d2df
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-07-31 22:48:26 -0400

    Create King.java

---
+++ src/main/java/com/chess/game/piece/standard/King.java
@@ -0,0 +1,64 @@
+package com.chess.game.piece.standard;
+
+import com.chess.game.Colour;
+import com.chess.game.Space2D;
+import com.chess.game.Vector2D;
+import com.chess.game.Vector2DUtil;
+import com.chess.game.movement.ActionRecord;
+import com.chess.game.piece.ChessPiece;
+import com.chess.game.piece.Piece;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class King implements ChessPiece {
+
+    private final Colour colour;
+    private Vector2D point;
+    private boolean hasMoved;
+
+    public King(Colour colour, Vector2D point) {
+        this.colour = colour;
+        this.point = point;
+        this.hasMoved = false;
+    }
+
+    @Override
+    public String getCode() {
+        return "K";
+    }
+
+    @Override
+    public Colour getColour() {
+        return this.colour;
+    }
+
+    @Override
+    public Set<Vector2D> getMoves(Space2D<Piece> board) {
+        Set<Vector2D> set = new HashSet<>();
+        set.add(Vector2DUtil.generateValidPointOrNull(board, this.point, this.colour, -1, 0)); // left
+        set.add(Vector2DUtil.generateValidPointOrNull(board, this.point, this.colour, -1, 1)); // top left
+        set.add(Vector2DUtil.generateValidPointOrNull(board, this.point, this.colour, 0, 1)); // top
+        set.add(Vector2DUtil.generateValidPointOrNull(board, this.point, this.colour, 1, 1)); // top right
+        set.add(Vector2DUtil.generateValidPointOrNull(board, this.point, this.colour, 1, 0)); // right
+        set.add(Vector2DUtil.generateValidPointOrNull(board, this.point, this.colour, 1, -1)); // bottom right
+        set.add(Vector2DUtil.generateValidPointOrNull(board, this.point, this.colour, 0, -1)); // bottom
+        set.add(Vector2DUtil.generateValidPointOrNull(board, this.point, this.colour, -1, -1)); // bottom left
+        if (!hasMoved) {
+            // todo: Add castling
+        }
+        set.remove(null); // remove any case of null
+        return set;
+    }
+
+    @Override
+    public boolean canMove(Space2D<Piece> board, Collection<ActionRecord> log, Vector2D destination) {
+        return this.getMoves(board).contains(destination);
+    }
+
+    @Override
+    public void move(Vector2D destination) {
+        this.point = destination;
+        this.hasMoved = true;
+    }
+}
