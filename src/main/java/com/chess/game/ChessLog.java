a69a0bbda84d1f50b811cb3597d1318338d630ab
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-01 11:23:24 -0400

    Create ChessLog.java

---
+++ src/main/java/com/chess/game/ChessLog.java
@@ -0,0 +1,17 @@
+package com.chess.game;
+
+import java.util.Collection;
+
+public interface ChessLog extends Collection<LogRecord> {
+
+    void push(LogRecord item);
+
+    LogRecord peek();
+
+    LogRecord pop();
+
+    void undo();
+
+    void redo();
+
+}
