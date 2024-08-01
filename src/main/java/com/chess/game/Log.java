d32fd63241c0882f491ae99c1cf198519c8ac5f1
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-07-31 23:09:55 -0400

    Update Log.java
    Implemented collection methods wrapping a Deque. Also has some stack methods for clarity of use.

--- src/main/java/com/chess/game/Log.java
+++ src/main/java/com/chess/game/Log.java
@@ -3,37 +3,134 @@ package com.chess.game;
 import com.chess.game.movement.ActionRecord;
 import com.chess.game.piece.Piece;
 import java.util.ArrayDeque;
+import java.util.Collection;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.List;
 
-public class Log {
+public class Log implements Collection<ActionRecord> {
 
-    private final Deque<ActionRecord> log;
+    private final Deque<ActionRecord> logStack;
+    private final Deque<ActionRecord> undoStack;
 
     public Log() {
-        this.log = new ArrayDeque<>();
+        this.logStack = new ArrayDeque<>();
+        this.undoStack = new ArrayDeque<>();
     }
 
     public Log(Space2D<Piece> board, List<String> logStrings) {
         this();
         for (String s : logStrings) {
             ActionRecord rec = new ActionRecord(board, s);
-            this.log.push(rec);
+            this.logStack.push(rec);
             // Manually move the piece on the board, ignoring all checks
             board.put(rec.getAction().getEnd(), board.remove(rec.getAction().getStart()));
         }
     }
 
-    // push
+    @Override
+    public boolean addAll(Collection<? extends ActionRecord> c) {
+        return this.logStack.addAll(c);
+    }
 
-    // peek
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return this.logStack.removeAll(c);
+    }
 
-    // pop
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return this.logStack.retainAll(c);
+    }
 
-    // undo
+    @Override
+    public void clear() {
+        this.logStack.clear();
+    }
 
-    // redo
+    @Override
+    public boolean equals(Object o) {
+        return this.logStack.equals(o);
+    }
 
-    // restore
+    @Override
+    public int hashCode() {
+        return this.logStack.hashCode();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return this.logStack.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return this.logStack.contains(c);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return this.logStack.contains(o);
+    }
+
+    @Override
+    public int size() {
+        return this.logStack.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.logStack.isEmpty();
+    }
+
+    @Override
+    public Iterator<ActionRecord> iterator() {
+        return this.logStack.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return this.logStack.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return this.logStack.toArray(a);
+    }
+
+    @Override
+    public boolean add(ActionRecord actionRecord) {
+        return this.logStack.add(actionRecord);
+    }
+
+    public ActionRecord peek() {
+        return this.logStack.peek();
+    }
+
+    public void push(ActionRecord actionRecord) {
+        this.logStack.push(actionRecord);
+    }
+
+    public ActionRecord pop() {
+        return this.logStack.pop();
+    }
+
+    public void undo() {
+        if (!this.logStack.isEmpty()) {
+            this.undoStack.push(this.logStack.pop());
+        }
+    }
+
+    public void redo() {
+        if (!this.undoStack.isEmpty()) {
+            this.logStack.push(this.undoStack.pop());
+        }
+    }
+
+    public void restore() {
+        while (!this.undoStack.isEmpty()) {
+            this.redo();
+        }
+    }
 
 }
