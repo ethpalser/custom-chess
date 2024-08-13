f57058a26ed9b575938260a1b15838455769e9f2
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-13 00:03:19 -0400

    Update MoveSet.java

--- src/main/java/com/ethpalser/chess/move/MoveSet.java
+++ src/main/java/com/ethpalser/chess/move/MoveSet.java
@@ -3,7 +3,6 @@ package com.ethpalser.chess.move;
 import com.ethpalser.chess.space.Point;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class MoveSet {
 
@@ -22,7 +21,7 @@ public class MoveSet {
     }
 
     public Move getMove(Point point) {
-        return this.moves.stream().filter(m -> point.equals(m.getPoint())).findFirst().orElse(null);
+        return this.moves.stream().filter(m -> m.getPath().toSet().contains(point)).findFirst().orElse(null);
     }
 
     public void addMove(Move move) {
@@ -30,7 +29,11 @@ public class MoveSet {
     }
 
     public Set<Point> getPoints() {
-        return this.moves.stream().map(Move::getPoint).collect(Collectors.toSet());
+        Set<Point> points = new HashSet<>();
+        for (Move m : this.moves) {
+            points.addAll(m.getPath().toSet());
+        }
+        return points;
     }
 
 }
