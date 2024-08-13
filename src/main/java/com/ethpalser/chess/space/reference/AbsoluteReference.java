c4b277e04f47efe1d922d3849d83e5106f2224a5
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-13 16:57:30 -0400

    Create AbsoluteReference.java

---
+++ src/main/java/com/ethpalser/chess/space/reference/AbsoluteReference.java
@@ -0,0 +1,34 @@
+package com.ethpalser.chess.space.reference;
+
+import com.ethpalser.chess.piece.Colour;
+import com.ethpalser.chess.space.Direction;
+import com.ethpalser.chess.space.Plane;
+import com.ethpalser.chess.space.Point;
+import java.util.List;
+
+public class AbsoluteReference<T> implements Reference<T> {
+
+    private final Point point;
+    private final Direction direction;
+    private final Colour colour;
+
+    public AbsoluteReference(Point point) {
+        this(point, Colour.WHITE, Direction.AT);
+    }
+
+    public AbsoluteReference(Point point, Colour colour, Direction direction) {
+        this.point = point;
+        this.direction = direction;
+        this.colour = colour;
+    }
+
+    @Override
+    public Location getLocation() {
+        return Location.VECTOR;
+    }
+
+    @Override
+    public List<T> getReferences(Plane<T> plane) {
+        return List.of(plane.get(this.point.shift(this.colour, this.direction)));
+    }
+}
