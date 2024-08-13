35f8f49cd51dcac5af73f93cd55998ab88dbee6b
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-13 16:44:50 -0400

    Update RelativeReference.java

--- src/main/java/com/ethpalser/chess/space/reference/RelativeReference.java
+++ src/main/java/com/ethpalser/chess/space/reference/RelativeReference.java
@@ -2,45 +2,47 @@ package com.ethpalser.chess.space.reference;
 
 import com.ethpalser.chess.board.CustomBoard;
 import com.ethpalser.chess.game.Action;
+import com.ethpalser.chess.piece.Colour;
 import com.ethpalser.chess.piece.Piece;
 import com.ethpalser.chess.piece.custom.CustomPiece;
-import com.ethpalser.chess.space.Path;
 import com.ethpalser.chess.space.Direction;
+import com.ethpalser.chess.space.Path;
+import com.ethpalser.chess.space.Plane;
 import com.ethpalser.chess.space.Point;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class RelativeReference {
+public class RelativeReference<T> implements Reference<T> {
 
     private final Location location;
     private final Direction direction;
-    private final Point vector;
-
-    public RelativeReference() {
-        this(Location.START);
-    }
+    private final Colour colour;
+    private final Point start;
+    private final Point end;
 
     public RelativeReference(Location location) {
         this(location, Direction.AT, null);
     }
 
-    public RelativeReference(Location location, Point vector) {
-        this(location, Direction.AT, vector);
+    public RelativeReference(Location location, Point point) {
+        this(location, Direction.AT, point);
     }
 
-    public RelativeReference(Location location, Direction direction) {
-        this(location, direction, null);
+    public RelativeReference(Location location, Direction direction, Point point) {
+        this(location, Colour.WHITE, direction, point, null);
     }
 
-    public RelativeReference(Location location, Direction direction, Point vector) {
+    public RelativeReference(Location location, Colour colour, Direction direction, Point point) {
+        this(location, colour, direction, point, null);
+    }
+
+    public RelativeReference(Location location, Colour colour, Direction direction, Point start, Point end) {
         this.location = location;
         this.direction = direction;
-
-        if (location == Location.VECTOR) {
-            this.vector = vector;
-        } else {
-            this.vector = null;
-        }
+        this.colour = colour;
+        this.start = start;
+        this.end = end;
     }
 
     /**
@@ -60,7 +62,7 @@ public class RelativeReference {
         }
         Point shiftedStart = action.getStart().shift(action.getColour(), this.direction);
         Point shiftedEnd = action.getEnd().shift(action.getColour(), this.direction);
-        Point shiftedReference = this.vector == null ? null : this.vector.shift(action.getColour(), this.direction);
+        Point shiftedReference = this.start == null ? null : this.start.shift(action.getColour(), this.direction);
 
         List<Piece> list = new ArrayList<>();
         Piece toAdd = null;
@@ -77,4 +79,25 @@ public class RelativeReference {
         return list;
     }
 
+    @Override
+    public Location getLocation() {
+        return this.location;
+    }
+
+    @Override
+    public List<T> getReferences(Plane<T> plane) {
+        if (plane == null) {
+            return List.of();
+        }
+        Point shiftedStart = this.start.shift(this.colour, this.direction);
+        Point shiftedEnd = this.end.shift(this.colour, this.direction);
+
+        return switch (this.location) {
+            case START, VECTOR -> List.of(plane.get(shiftedStart));
+            case DESTINATION -> List.of(plane.get(shiftedEnd));
+            case PATH_TO_DESTINATION, PATH_TO_VECTOR -> new Path(shiftedStart, shiftedEnd).toSet().stream().map(plane::get)
+                    .collect(Collectors.toList());
+            default -> List.of();
+        };
+    }
 }
