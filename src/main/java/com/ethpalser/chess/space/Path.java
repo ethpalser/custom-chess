ded5387c02cabcba61db7eb9a40846b6ba35c3b5
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-12 20:23:28 -0400

    Move PathType into Path.java as private enum

--- src/main/java/com/ethpalser/chess/space/Path.java
+++ src/main/java/com/ethpalser/chess/space/Path.java
@@ -51,7 +51,7 @@ public class Path implements Iterable<Point> {
         if (start.equals(end)) {
             pathType = PathType.CUSTOM;
         } else {
-            pathType = PathType.findType(start, end);
+            pathType = PathType.fromPoints(start, end);
         }
 
         int x = start.getX();
@@ -173,6 +173,28 @@ public class Path implements Iterable<Point> {
             }
         }
         return sb.toString();
+    }
+
+    private enum PathType {
+        VERTICAL,
+        HORIZONTAL,
+        DIAGONAL,
+        CUSTOM;
+
+        public static Path.PathType fromPoints(Point start, Point end) {
+            int diffX = Math.abs(end.getX() - start.getX());
+            int diffY = Math.abs(end.getY() - start.getY());
+
+            if (diffX == 0) {
+                return VERTICAL;
+            } else if (diffY == 0) {
+                return HORIZONTAL;
+            } else if (diffX == diffY) {
+                return DIAGONAL;
+            } else {
+                return CUSTOM;
+            }
+        }
     }
 
 }
