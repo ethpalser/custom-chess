e3dd8db1213b4053d0ef665dabd2ae5493685c0b
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-13 00:42:45 -0400

    Update MoveSet.java
    Marked original constructor as depreciated and created new constructor

--- src/main/java/com/ethpalser/chess/move/MoveSet.java
+++ src/main/java/com/ethpalser/chess/move/MoveSet.java
@@ -1,36 +1,51 @@
 package com.ethpalser.chess.move;
 
+import com.ethpalser.chess.space.Path;
 import com.ethpalser.chess.space.Point;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class MoveSet {
 
-    private final Set<Move> moves;
+    private final Set<Move> set;
 
+    @Deprecated(since = "2024-08-13")
     public MoveSet(Set<Point> points) {
-        Set<Move> moveSet = new HashSet<>();
+        Set<Move> moves = new HashSet<>();
         for (Point p : points) {
-            moveSet.add(new Move(p));
+            moves.add(new Move(p));
         }
-        this.moves = moveSet;
+        this.set = moves;
+    }
+
+    public MoveSet(Path... paths) {
+        Set<Move> moves = new HashSet<>();
+        for (Path path : paths) {
+            moves.add(new Move(path));
+        }
+        this.set = moves;
+    }
+
+    public MoveSet(Move... moves) {
+        this.set = new HashSet<>(Arrays.asList(moves));
     }
 
     public Set<Move> toSet() {
-        return this.moves;
+        return this.set;
     }
 
     public Move getMove(Point point) {
-        return this.moves.stream().filter(m -> m.getPath().toSet().contains(point)).findFirst().orElse(null);
+        return this.set.stream().filter(m -> m.getPath().toSet().contains(point)).findFirst().orElse(null);
     }
 
     public void addMove(Move move) {
-        this.moves.add(move);
+        this.set.add(move);
     }
 
     public Set<Point> getPoints() {
         Set<Point> points = new HashSet<>();
-        for (Move m : this.moves) {
+        for (Move m : this.set) {
             points.addAll(m.getPath().toSet());
         }
         return points;
