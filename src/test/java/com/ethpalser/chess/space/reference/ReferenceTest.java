b7d952f358c0434bb811801c163e4eebc6a2730c
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-14 15:27:26 -0400

    Update ReferenceTest.java

--- src/test/java/com/ethpalser/chess/space/reference/ReferenceTest.java
+++ src/test/java/com/ethpalser/chess/space/reference/ReferenceTest.java
@@ -7,21 +7,21 @@ import com.ethpalser.chess.space.Point;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-public class ReferenceTest {
+class ReferenceTest {
 
     @Test
-    void pieceRef_getReference_givenAtLocationAndNotMoved_thenIsItself() {
+    void pieceRef_getReferences_givenAtLocationAndNotMoved_thenIsItself() {
         StandardBoard board = new StandardBoard();
         // Given
         Piece piece = board.getPiece(4, 1); // e1 pawn
         // When
-        PieceReference ref = new PieceReference(piece, Direction.AT);
+        PieceReference ref = new PieceReference(piece);
         // Then
         assertTrue(ref.getReferences(board.getPieces()).contains(piece));
     }
 
     @Test
-    void pieceRef_getReference_givenAtLocationAndMoved_thenIsItself() {
+    void pieceRef_getReferences_givenAtLocationAndMoved_thenIsItself() {
         StandardBoard board = new StandardBoard();
         // Given
         Piece piece = board.getPiece(4, 1); // e1 pawn
@@ -32,16 +32,51 @@ public class ReferenceTest {
         assertTrue(ref.getReferences(board.getPieces()).contains(piece));
     }
 
-    void pieceRef_getReference_givenBackOfLocationAndMovedUpOne_thenIsNothing() {
-
+    void pieceRef_getReferences_givenBackOfLocationAndMovedUpOne_thenIsEmpty() {
+        StandardBoard board = new StandardBoard();
+        // Given
+        Piece piece = board.getPiece(4, 1); // e1 pawn
+        // When
+        PieceReference ref = new PieceReference(piece, Direction.BACK);
+        board.movePiece(new Point(4, 1), new Point(4, 2));
+        // Then
+        assertTrue(ref.getReferences(board.getPieces()).isEmpty());
     }
 
-    void pieceRef_getReference_givenRightOfLocationAndPawnToRight_thenIsPawn() {
-
+    @Test
+    void pieceRef_getReferences_givenRightOfLocationAndPawnToRight_thenIsPawn() {
+        StandardBoard board = new StandardBoard();
+        // Given
+        Piece refPiece = board.getPiece(4, 1); // e1 pawn
+        Piece rightPiece = board.getPiece(5, 1); // f1 pawn
+        // When
+        PieceReference ref = new PieceReference(refPiece, Direction.RIGHT);
+        board.movePiece(new Point(4, 1), new Point(4, 2));
+        board.movePiece(new Point(5, 1), new Point(5, 2));
+        // Then
+        assertTrue(ref.getReferences(board.getPieces()).contains(rightPiece));
     }
 
-    void pieceRef_getReference_givenLeftThreeOfLocationAndRookToLeftThree_thenIsRook() {
-
+    @Test
+    void pieceRef_getReferences_givenLeftFourOfKingAtStart_thenIsRook() {
+        StandardBoard board = new StandardBoard();
+        // Given
+        Piece refPiece = board.getPiece(4, 0); // d0 king
+        Piece expected = board.getPiece(0, 0); // a0 rook
+        // When
+        PieceReference ref = new PieceReference(refPiece, Direction.LEFT, 4);
+        // Then
+        assertTrue(ref.getReferences(board.getPieces()).contains(expected));
     }
 
+    @Test
+    void pieceRef_getReferences_givenOutOfBounds_thenIsEmpty() {
+        StandardBoard board = new StandardBoard();
+        // Given
+        Piece refPiece = board.getPiece(4, 0); // d0 king
+        // When
+        PieceReference ref = new PieceReference(refPiece, Direction.BACK, 2);
+        // Then
+        assertTrue(ref.getReferences(board.getPieces()).isEmpty());
+    }
 }
