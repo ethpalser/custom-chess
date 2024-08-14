08435988b87a524ae9441a18090e70e28fc33019
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-14 15:59:36 -0400

    Update ReferenceTest.java

--- src/test/java/com/ethpalser/chess/space/reference/ReferenceTest.java
+++ src/test/java/com/ethpalser/chess/space/reference/ReferenceTest.java
@@ -1,13 +1,44 @@
 package com.ethpalser.chess.space.reference;
 
+import com.ethpalser.chess.board.Board;
 import com.ethpalser.chess.board.StandardBoard;
 import com.ethpalser.chess.piece.Piece;
 import com.ethpalser.chess.space.Direction;
 import com.ethpalser.chess.space.Point;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class ReferenceTest {
+
+    @Test
+    void absoluteRef_getReferences_givenLocationEmpty_thenIsEmpty() {
+        // Given
+        Board board = new StandardBoard();
+        Reference<Piece> reference = new AbsoluteReference<>(new Point(4, 3));
+        // Then
+        assertTrue(reference.getReferences(board.getPieces()).isEmpty());
+    }
+
+    @Test
+    void absoluteRef_getReferences_givenLocationNotEmpty_thenHasPiece() {
+        // Given
+        Board board = new StandardBoard();
+        Reference<Piece> reference = new AbsoluteReference<>(new Point(0, 0));
+        // Then
+        assertFalse(reference.getReferences(board.getPieces()).isEmpty());
+    }
+
+    @Test
+    void absoluteRef_getReferences_givenPieceMovedOntoLocation_thenHasPiece() {
+        // Given
+        Board board = new StandardBoard();
+        Reference<Piece> reference = new AbsoluteReference<>(new Point(4, 3));
+        board.movePiece(new Point(4, 1), new Point(4, 3));
+        // Then
+        assertFalse(reference.getReferences(board.getPieces()).isEmpty());
+        assertTrue(reference.getReferences(board.getPieces()).contains(board.getPiece(4, 3)));
+    }
 
     @Test
     void pieceRef_getReferences_givenAtLocationAndNotMoved_thenIsItself() {
