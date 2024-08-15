ab87c35b62ceabee710ff905321b32641029f760
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-15 15:53:21 -0400

    Update Conditional.java

--- src/main/java/com/ethpalser/chess/move/custom/condition/Conditional.java
+++ src/main/java/com/ethpalser/chess/move/custom/condition/Conditional.java
@@ -1,12 +1,13 @@
 package com.ethpalser.chess.move.custom.condition;
 
 import com.ethpalser.chess.space.Plane;
+import com.ethpalser.chess.space.Positional;
 
 /**
  * Conditional describes an object that has conditions, rule or criteria, that it contains. The criteria will take
  * the current state of the board to determine if the expected criteria is met.
  */
-public interface Conditional<T> {
+public interface Conditional<T extends Positional> {
 
     boolean isExpected(Plane<T> plane);
 
