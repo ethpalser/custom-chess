6859d73e6e216acb2ec9e00375ec5ce6db62a103
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-08-15 19:03:07 -0400

    Update CustomBoard.java

--- src/main/java/com/ethpalser/chess/board/CustomBoard.java
+++ src/main/java/com/ethpalser/chess/board/CustomBoard.java
@@ -1,18 +1,17 @@
 package com.ethpalser.chess.board;
 
+import com.ethpalser.chess.exception.IllegalActionException;
+import com.ethpalser.chess.move.Move;
 import com.ethpalser.chess.piece.Colour;
 import com.ethpalser.chess.piece.Piece;
 import com.ethpalser.chess.piece.custom.CustomPiece;
 import com.ethpalser.chess.piece.custom.CustomPieceFactory;
 import com.ethpalser.chess.piece.custom.PieceType;
-import com.ethpalser.chess.space.Path;
 import com.ethpalser.chess.space.Plane;
 import com.ethpalser.chess.space.Point;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class CustomBoard implements Board {
 
@@ -41,18 +40,6 @@ public class CustomBoard implements Board {
     @Override
     public Plane<Piece> getPieces() {
         return pieces;
-    }
-
-    public List<Piece> getPieces(Path path) {
-        if (path == null) {
-            List<Piece> list = new LinkedList<>();
-            for (Piece piece : this.pieces) {
-                list.add(piece);
-            }
-            return list;
-        } else {
-            return path.toSet().stream().map(this::getPiece).collect(Collectors.toList());
-        }
     }
 
     @Override
@@ -85,7 +72,22 @@ public class CustomBoard implements Board {
         if (start == null || end == null) {
             throw new NullPointerException();
         }
-        this.addPiece(end, this.getPiece(start));
+        CustomPiece piece = (CustomPiece) this.getPiece(start);
+        Move move = piece.getMoves(this, null, null).getMove(end);
+        if (move == null) {
+            throw new IllegalActionException("this piece cannot move to " + end);
+        }
+
+        this.pieces.remove(start);
+        this.pieces.put(end, piece);
+        piece.move(end);
+
+        move.getFollowUpMove().ifPresent(m -> {
+            Piece followUp = m.getStartObject();
+            this.pieces.remove(m.getStart());
+            this.pieces.put(m.getEnd(), followUp);
+            this.pieces.remove(null); // If the piece is meant to be removed it was put here
+        });
     }
 
     @Override
