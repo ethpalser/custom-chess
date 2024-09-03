251e200beaada8dac271edffcf4d99ee740c95cc
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-09-03 18:35:55 -0400

    Create BoardView.java

---
+++ src/main/java/com/ethpalser/chess/game/view/BoardView.java
@@ -0,0 +1,34 @@
+package com.ethpalser.chess.game.view;
+
+import com.ethpalser.chess.piece.Piece;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BoardView {
+
+    private final int width;
+    private final int length;
+    private final List<String> pieces;
+
+    BoardView(List<Piece> pieces, int width, int length) {
+        if (pieces == null) {
+            this.pieces = List.of();
+        } else {
+            this.pieces = pieces.stream().map(Piece::toString).collect(Collectors.toList());
+        }
+        this.width = width;
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public List<String> getPieces() {
+        return pieces;
+    }
+}
