e451e4d24f76f91f7c5b45a2e08f77600fee8f94
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-09-03 18:35:24 -0400

    Create ActionView.java

---
+++ src/main/java/com/ethpalser/chess/game/view/ActionView.java
@@ -0,0 +1,30 @@
+package com.ethpalser.chess.game.view;
+
+import com.ethpalser.chess.space.custom.reference.Reference;
+
+public class ActionView {
+
+    private final ReferenceView target;
+    private final ReferenceView destination;
+
+    public ActionView(Reference<?> target, Reference<?> destination) {
+        if (target == null) {
+            this.target = null;
+        } else {
+            this.target = target.toView();
+        }
+        if (destination == null) {
+            this.destination = null;
+        } else {
+            this.destination = destination.toView();
+        }
+    }
+
+    public ReferenceView getTarget() {
+        return target;
+    }
+
+    public ReferenceView getDestination() {
+        return destination;
+    }
+}
