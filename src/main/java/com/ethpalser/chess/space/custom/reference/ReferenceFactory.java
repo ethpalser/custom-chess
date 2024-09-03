acc92d840a41d7e6decdfc27558a556ce0909af7
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-09-03 03:53:12 -0400

    Update ReferenceFactory.java

--- src/main/java/com/ethpalser/chess/space/custom/reference/ReferenceFactory.java
+++ src/main/java/com/ethpalser/chess/space/custom/reference/ReferenceFactory.java
@@ -2,33 +2,28 @@ package com.ethpalser.chess.space.custom.reference;
 
 import com.ethpalser.chess.board.Board;
 import com.ethpalser.chess.log.Log;
-import com.ethpalser.chess.piece.Colour;
 import com.ethpalser.chess.piece.Piece;
 import com.ethpalser.chess.space.Direction;
 import com.ethpalser.chess.space.Point;
 import com.ethpalser.chess.space.custom.Location;
 
-public class ChessReferenceFactory {
+public class ReferenceFactory {
 
     private final Board board;
     private final Log<Point, Piece> log;
 
-    public ChessReferenceFactory(Board board, Log<Point, Piece> log) {
+    public ReferenceFactory(Board board, Log<Point, Piece> log) {
         this.board = board;
         this.log = log;
     }
 
-    public Reference<Piece> build(Location location, Colour colour, Direction direction, Point start, int shiftX,
-            int shiftY) {
+    public Reference<Piece> build(Location location, Direction direction, Point start, int shiftX, int shiftY) {
         switch (location) {
             case PIECE -> {
                 return new PieceReference(board.getPiece(start), direction, shiftX, shiftY);
             }
-            case POINT, PATH_START -> {
-                return new AbsoluteReference<>(start, colour, direction);
-            }
-            case PATH_END -> {
-                return new AbsoluteReference<>(this.pathEnd(start, shiftX, shiftY), colour, direction);
+            case POINT -> {
+                return new AbsoluteReference<>(start);
             }
             case LAST_MOVED -> {
                 return new LogReference<>(this.log);
