41acaa904e545d242a3620752c0430068d8ed25d
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-09-03 18:01:29 -0400

    Update CustomMoveType.java

--- src/main/java/com/ethpalser/chess/move/custom/CustomMoveType.java
+++ src/main/java/com/ethpalser/chess/move/custom/CustomMoveType.java
@@ -1,7 +1,28 @@
 package com.ethpalser.chess.move.custom;
 
 public enum CustomMoveType {
-    JUMP,
-    ADVANCE,
-    CHARGE
+    JUMP("jump"),
+    ADVANCE("advance"),
+    CHARGE("charge");
+
+    private final String display;
+
+    CustomMoveType(String display) {
+        this.display = display;
+    }
+
+    @Override
+    public String toString() {
+        return this.display;
+    }
+
+    public static CustomMoveType fromString(String string) {
+        for (CustomMoveType e : CustomMoveType.values()) {
+            if (e.display.equalsIgnoreCase(string)) {
+                return e;
+            }
+        }
+        return ADVANCE;
+    }
+
 }
