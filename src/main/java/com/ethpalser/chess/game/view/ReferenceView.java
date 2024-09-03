4abcd4a51df290dfcf5eba0f163a19e5df13b574
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-09-03 18:35:39 -0400

    Create ReferenceView.java

---
+++ src/main/java/com/ethpalser/chess/game/view/ReferenceView.java
@@ -0,0 +1,36 @@
+package com.ethpalser.chess.game.view;
+
+import com.ethpalser.chess.space.Point;
+import com.ethpalser.chess.space.custom.Location;
+import java.util.Objects;
+
+public class ReferenceView {
+
+    private final Location location;
+    private final Point point;
+    private final int xOffset;
+    private final int yOffset;
+
+    public ReferenceView(Location location, Point point, int xOffset, int yOffset) {
+        this.location = Objects.requireNonNullElse(location, Location.POINT);
+        this.point = point;
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public int getXOffset() {
+        return xOffset;
+    }
+
+    public int getYOffset() {
+        return yOffset;
+    }
+}
