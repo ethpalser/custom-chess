cf94e3b16dbc7f321c38a7914be12de03d628e13
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-12-21 21:22:48 -0500

    Create core GameStates: Ready, Await, and End

---
+++ src/main/java/com/ethpalser/chess/game/state/EndState.java
@@ -0,0 +1,39 @@
+package com.ethpalser.chess.game.state;
+
+import com.ethpalser.chess.exception.IllegalActionException;
+import com.ethpalser.chess.game.GameContext;
+import com.ethpalser.chess.game.event.GameEvent;
+import java.util.List;
+
+public class EndState implements GameState {
+
+    private final GameContext context;
+
+    public EndState(GameContext context) {
+        if (context == null) {
+            throw new IllegalArgumentException("One or more arguments is null");
+        }
+        this.context = context;
+    }
+
+    @Override
+    public GameState update(GameEvent event) {
+        throw new IllegalActionException();
+    }
+
+    @Override
+    public Iterable<GameEvent> updates() {
+        return List.of();
+    }
+
+    @Override
+    public GameState undo() {
+        this.context.getLog().undo();
+        return new ReadyState(this.context);
+    }
+
+    @Override
+    public GameState redo() {
+        throw new IllegalActionException();
+    }
+}
---
+++ src/main/java/com/ethpalser/chess/game/state/ReadyState.java
@@ -0,0 +1,47 @@
+package com.ethpalser.chess.game.state;
+
+import com.ethpalser.chess.exception.IllegalActionException;
+import com.ethpalser.chess.game.GameContext;
+import com.ethpalser.chess.game.event.EventType;
+import com.ethpalser.chess.game.event.GameEvent;
+
+public class ReadyState implements GameState {
+
+    private final GameContext context;
+
+    public ReadyState(GameContext context) {
+        if (context == null) {
+            throw new IllegalArgumentException("One or more arguments is null");
+        }
+        this.context = context;
+    }
+
+    @Override
+    public GameState update(GameEvent event) {
+        if (EventType.MOVE.equals(event.type())) {
+            event.execute(this.context);
+            GamePrompt prompt = this.context.getPrompt();
+            if (prompt != null) {
+                return new AwaitState(this.context, prompt.eventType(), prompt.choices());
+            }
+        } else {
+            throw new IllegalActionException("Event not supported by the current game state");
+        }
+        return new ReadyState(this.context);
+    }
+
+    @Override
+    public Iterable<GameEvent> updates() {
+        return null;
+    }
+
+    @Override
+    public GameState undo() {
+        return null;
+    }
+
+    @Override
+    public GameState redo() {
+        return null;
+    }
+}
