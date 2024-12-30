803a33b2f018a8ca7fc0b4340ceb4d0f7bf6047c
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-12-30 00:17:22 -0500

    Update ChessNotationFormat.java

--- src/main/java/com/ethpalser/chess/move/notation/ChessNotationFormat.java
+++ src/main/java/com/ethpalser/chess/move/notation/ChessNotationFormat.java
@@ -1,6 +1,19 @@
 package com.ethpalser.chess.move.notation;
 
-public enum ChessNotationFormat {
-    VERBOSE,
-    COORDINATE,
+public interface ChessNotationFormat {
+
+    String format(ChessRecord chessRecord);
+
+    /**
+     * Formats the chess record with an alias provided to replace a portion of its string. This format should know
+     * how to handle the alias, or use everything provided by the record.
+     *
+     * @param chessRecord Record of everything involved in a change of state for a chess game
+     * @param alias String representing a simplified version of a state change.
+     * @return String using the alias
+     */
+    String format(ChessRecord chessRecord, ChessNotationAlias alias);
+
+    ChessRecord parse(String chessNotation);
+
 }
