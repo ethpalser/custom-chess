7b36ed8a1967ca5d79b48e7cac7ab7df50f21ed9
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-02 02:02:07 -0500

    Rearranged code in MovementSpec.java

--- src/main/java/com/ethpalser/chess/move/custom/MovementSpec.java
+++ src/main/java/com/ethpalser/chess/move/custom/MovementSpec.java
@@ -1,13 +1,10 @@
 package com.ethpalser.chess.move.custom;
 
 import com.ethpalser.chess.board.Board;
-import com.ethpalser.chess.log.Log;
 import com.ethpalser.chess.log.LogEntry;
-import com.ethpalser.chess.log.custom.ReferenceLogEntry;
 import com.ethpalser.chess.move.Move;
 import com.ethpalser.chess.move.Movement;
 import com.ethpalser.chess.move.custom.condition.Conditional;
-import com.ethpalser.chess.move.custom.condition.ConditionalFactory;
 import com.ethpalser.chess.move.map.ThreatMap;
 import com.ethpalser.chess.piece.Colour;
 import com.ethpalser.chess.piece.Piece;
@@ -16,8 +13,6 @@ import com.ethpalser.chess.space.Coordinate;
 import com.ethpalser.chess.space.Path;
 import com.ethpalser.chess.space.Plane;
 import com.ethpalser.chess.space.Point;
-import com.ethpalser.chess.space.custom.reference.ReferenceFactory;
-import com.ethpalser.chess.view.ConditionalView;
 import com.ethpalser.chess.view.MoveView;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -37,76 +32,6 @@ public class MovementSpec {
     private final List<Conditional<Piece>> conditions;
     private final LogEntry<Coordinate, Piece> followUp;
 
-    public static class Builder {
-        // required
-        private final Path path;
-        private final MovementType moveType;
-        // optional
-        private boolean mirrorXAxis = true;
-        private boolean mirrorYAxis = true;
-        private boolean isSpecificQuadrant = false;
-        private boolean isAttack = true;
-        private boolean isMove = true;
-        private List<Conditional<Piece>> conditions = List.of();
-        private LogEntry<Coordinate, Piece> followUp = null;
-
-        public Builder(Path path, MovementType moveType) {
-            this.path = Objects.requireNonNullElse(path, new Path(List.of()));
-            this.moveType = Objects.requireNonNullElse(moveType, MovementType.ADVANCE);
-        }
-
-        public Builder isMirrorXAxis(Boolean bool) {
-            this.mirrorXAxis = Objects.requireNonNullElse(bool, false);
-            return this;
-        }
-
-        public Builder isMirrorYAxis(Boolean bool) {
-            this.mirrorYAxis = Objects.requireNonNullElse(bool, false);
-            return this;
-        }
-
-        public Builder isSpecificQuadrant(Boolean bool) {
-            this.isSpecificQuadrant = Objects.requireNonNullElse(bool, false);
-            return this;
-        }
-
-        public Builder isAttack(Boolean bool) {
-            this.isAttack = Objects.requireNonNullElse(bool, true);
-            return this;
-        }
-
-        public Builder isMove(Boolean bool) {
-            this.isMove = Objects.requireNonNullElse(bool, true);
-            return this;
-        }
-
-        public Builder conditions(List<Conditional<Piece>> conditions) {
-            this.conditions = conditions;
-            return this;
-        }
-
-        public Builder followUp(LogEntry<Coordinate, Piece> followUp) {
-            this.followUp = followUp;
-            return this;
-        }
-
-        public MovementSpec build() {
-            return new MovementSpec(this);
-        }
-    }
-
-    MovementSpec(Builder builder) {
-        this.pathBase = builder.path;
-        this.moveType = builder.moveType;
-        this.mirrorXAxis = builder.mirrorXAxis;
-        this.mirrorYAxis = builder.mirrorYAxis;
-        this.isSpecificQuadrant = builder.isSpecificQuadrant;
-        this.isAttack = builder.isAttack;
-        this.isMove = builder.isMove;
-        this.conditions = builder.conditions;
-        this.followUp = builder.followUp;
-    }
-
     public MovementSpec(Path path, MovementType moveType, boolean mirrorXAxis, boolean mirrorYAxis) {
         this.pathBase = path;
         this.moveType = moveType;
@@ -119,38 +44,16 @@ public class MovementSpec {
         this.followUp = null;
     }
 
-    public MovementSpec(Board<Coordinate> board, Log<Coordinate, Piece> log, MoveView view) {
-        if (view == null) {
-            this.pathBase = null;
-            this.moveType = null;
-            this.mirrorXAxis = false;
-            this.mirrorYAxis = false;
-            this.isSpecificQuadrant = false;
-            this.isMove = true;
-            this.isAttack = true;
-            this.conditions = List.of();
-            this.followUp = null;
-        } else {
-            this.pathBase = new Path(view.getBase());
-            this.moveType = view.getType();
-            this.mirrorXAxis = view.isMirrorXAxis();
-            this.mirrorYAxis = view.isMirrorYAxis();
-            this.isSpecificQuadrant = view.isOnlySpecificQuadrant();
-            this.isMove = view.isMove();
-            this.isAttack = view.isAttack();
-            List<Conditional<Piece>> conditionalList = new ArrayList<>();
-            ConditionalFactory cFactory = new ConditionalFactory(board, log);
-            for (ConditionalView cv : view.getConditions()) {
-                conditionalList.add(cFactory.build(cv));
-            }
-            this.conditions = conditionalList;
-            ReferenceFactory rFactory = new ReferenceFactory(board, log);
-            this.followUp = new ReferenceLogEntry<>(
-                    board,
-                    rFactory.build(view.getFollowUp().getTarget()),
-                    rFactory.build(view.getFollowUp().getDestination())
-            );
-        }
+    private MovementSpec(Builder builder) {
+        this.pathBase = builder.path;
+        this.moveType = builder.moveType;
+        this.mirrorXAxis = builder.mirrorXAxis;
+        this.mirrorYAxis = builder.mirrorYAxis;
+        this.isSpecificQuadrant = builder.isSpecificQuadrant;
+        this.isAttack = builder.isAttack;
+        this.isMove = builder.isMove;
+        this.conditions = builder.conditions;
+        this.followUp = builder.followUp;
     }
 
     public List<Movement> toMovementList(Board<Coordinate> board, ThreatMap threatMap, Colour colour, Point offset,
@@ -294,5 +197,63 @@ public class MovementSpec {
             }
         }
         return true;
+    }
+
+    public static class Builder {
+        // required
+        private final Path path;
+        private final MovementType moveType;
+        // optional
+        private boolean mirrorXAxis = true;
+        private boolean mirrorYAxis = true;
+        private boolean isSpecificQuadrant = false;
+        private boolean isAttack = true;
+        private boolean isMove = true;
+        private List<Conditional<Piece>> conditions = List.of();
+        private LogEntry<Coordinate, Piece> followUp = null;
+
+        public Builder(Path path, MovementType moveType) {
+            this.path = Objects.requireNonNullElse(path, new Path(List.of()));
+            this.moveType = Objects.requireNonNullElse(moveType, MovementType.ADVANCE);
+        }
+
+        public Builder isMirrorXAxis(Boolean bool) {
+            this.mirrorXAxis = Objects.requireNonNullElse(bool, false);
+            return this;
+        }
+
+        public Builder isMirrorYAxis(Boolean bool) {
+            this.mirrorYAxis = Objects.requireNonNullElse(bool, false);
+            return this;
+        }
+
+        public Builder isSpecificQuadrant(Boolean bool) {
+            this.isSpecificQuadrant = Objects.requireNonNullElse(bool, false);
+            return this;
+        }
+
+        public Builder isAttack(Boolean bool) {
+            this.isAttack = Objects.requireNonNullElse(bool, true);
+            return this;
+        }
+
+        public Builder isMove(Boolean bool) {
+            this.isMove = Objects.requireNonNullElse(bool, true);
+            return this;
+        }
+
+        public Builder conditions(List<Conditional<Piece>> conditions) {
+            this.conditions = conditions;
+            return this;
+        }
+
+        public Builder followUp(LogEntry<Coordinate, Piece> followUp) {
+            this.followUp = followUp;
+            return this;
+        }
+
+        public MovementSpec build() {
+            return new MovementSpec(this);
+        }
     }
 }
