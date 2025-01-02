2cefd49c5ca7037ba570a99b9022b8c302c32453
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-02 01:39:52 -0500

    Update MoveSet.java

--- src/main/java/com/ethpalser/chess/move/MoveSet.java
+++ src/main/java/com/ethpalser/chess/move/MoveSet.java
@@ -1,18 +1,28 @@
 package com.ethpalser.chess.move;
 
+import com.ethpalser.chess.space.Coordinate;
 import com.ethpalser.chess.space.Path;
+import com.ethpalser.chess.space.PathReport;
 import com.ethpalser.chess.space.Point;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class MoveSet {
 
     private final Set<Movement> set;
+    private final Collection<Coordinate> attacks;
+    private final Collection<Coordinate> defends;
 
     public MoveSet(Set<Movement> moves) {
         this.set = moves;
+        // Legacy code handles these differently and is not supported by this constructor
+        this.attacks = List.of();
+        this.defends = List.of();
     }
 
     public MoveSet(Point... points) {
@@ -24,6 +34,9 @@ public class MoveSet {
         }
         moves.remove(null);
         this.set = moves;
+        // Legacy code handles these differently and is not supported by this constructor
+        this.attacks = List.of();
+        this.defends = List.of();
     }
 
     public MoveSet(Path... paths) {
@@ -35,10 +48,35 @@ public class MoveSet {
         }
         moves.remove(null);
         this.set = moves;
+        // Legacy code handles these differently and is not supported by this constructor
+        this.attacks = List.of();
+        this.defends = List.of();
     }
 
     public MoveSet(Movement... moves) {
         this.set = new HashSet<>(Arrays.asList(moves));
+        // Legacy code handles these differently and is not supported by this constructor
+        this.attacks = List.of();
+        this.defends = List.of();
+    }
+
+    public MoveSet(PathReport... pathReports) {
+        Set<Movement> movements = new HashSet<>();
+        Collection<Coordinate> attackList = new ArrayList<>();
+        Collection<Coordinate> defendList = new ArrayList<>();
+        for (PathReport report : pathReports) {
+            if (!report.path().isEmpty()) {
+                movements.add(new Move(report.path()));
+            }
+            if (report.status().equals(PathReport.Status.BLOCKED_BY_OPPONENT)) {
+                attackList.add(report.lastChecked());
+            } else if (report.status().equals(PathReport.Status.BLOCKED_BY_ALLY)) {
+                defendList.add(report.lastChecked());
+            }
+        }
+        this.set = movements;
+        this.attacks = attackList;
+        this.defends = defendList;
     }
 
     public Set<Movement> toSet() {
