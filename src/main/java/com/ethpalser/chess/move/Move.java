8f73ea7ea87f02b220aa2ba9ba1021cb4ddcf72c
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-05 03:09:20 -0500

    Update Move.java

--- src/main/java/com/ethpalser/chess/move/Move.java
+++ src/main/java/com/ethpalser/chess/move/Move.java
@@ -1,10 +1,6 @@
 package com.ethpalser.chess.move;
 
-import com.ethpalser.chess.log.LogEntry;
-import com.ethpalser.chess.piece.Piece;
 import com.ethpalser.chess.space.Coordinate;
-import com.ethpalser.chess.space.Direction;
-import com.ethpalser.chess.space.Location;
 import com.ethpalser.chess.space.Path;
 import com.ethpalser.chess.space.Point;
 import com.ethpalser.chess.space.Reference;
@@ -12,21 +8,14 @@ import com.ethpalser.chess.space.Reference;
 public record Move(Path path, FollowUp followUp) {
 
     public Move(Coordinate point) {
-        this(new Path((Point) point), (FollowUp) null);
+        this(new Path((Point) point), null);
     }
 
     public Move(Path path) {
-        this(path, (FollowUp) null);
-    }
-
-    @Deprecated(since = "2025-01-01")
-    public Move(Path path, LogEntry<Coordinate, Piece> followUpMove) {
-        this(path, followUpMove == null ? null : new FollowUp(
-                new Reference(Location.POINT, Direction.AT, followUpMove.getStart()),
-                new Path((Point) followUpMove.getEnd())
-        ));
+        this(path, null);
     }
 
     public record FollowUp(Reference reference, Path path) {
+
     }
 }
