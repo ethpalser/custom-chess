78d4048620261660af65abe1b987c6fc2f83a453
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-07 17:37:21 -0500

    Simplified Location.java

--- src/main/java/com/ethpalser/chess/space/Location.java
+++ src/main/java/com/ethpalser/chess/space/Location.java
@@ -1,27 +1,8 @@
 package com.ethpalser.chess.space;
 
 public enum Location {
-    LAST_MOVED("last moved"),
-    PATH("path"),
-    POINT("point");
-
-    private final String displayName;
-
-    Location(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @Override
-    public String toString() {
-        return this.displayName;
-    }
-
-    public static Location fromString(String string) {
-        for (Location location : Location.values()) {
-            if (location.displayName.equals(string)) {
-                return location;
-            }
-        }
-        return POINT; // Default
-    }
+    LAST_MOVED,
+    PATH,
+    POINT,
+    EDGE;
 }
