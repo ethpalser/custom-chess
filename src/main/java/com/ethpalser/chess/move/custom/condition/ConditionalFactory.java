0e127aab90f3e42094727f3429d47bb42f363572
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-07 16:23:35 -0500

    Create ConditionalFactory.java

---
+++ src/main/java/com/ethpalser/chess/move/custom/condition/ConditionalFactory.java
@@ -0,0 +1,47 @@
+package com.ethpalser.chess.move.custom.condition;
+
+public class ConditionalFactory {
+
+    private static ConditionalFactory factory;
+
+    private ConditionalFactory() {
+    }
+
+    public static ConditionalFactory getInstance() {
+        if (factory == null) {
+            factory = new ConditionalFactory();
+        }
+        return factory;
+    }
+
+    public Conditional create(ConditionalOptions spec) {
+        if (spec == null) {
+            return null;
+        }
+        switch (spec.type()) {
+            case PIECE -> {
+                if (spec.optional() != null) {
+                    return new PieceCompareConditional(spec.primary(), spec.operator(), spec.optional());
+                } else if (spec.property() != null) {
+                    return new PieceStateConditional(spec.primary(), spec.operator(), spec.property(), spec.expected());
+                }
+                return fail();
+
+            }
+            case LOG -> {
+                if (spec.property() != null) {
+                    return new GameHistoryConditional(spec.operator(), spec.property(), spec.expected());
+                }
+                return fail();
+            }
+            default -> {
+                return fail();
+            }
+        }
+    }
+
+    private Conditional fail() {
+        return (context, appliedTo) -> false;
+    }
+
+}
