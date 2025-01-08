eb8935552cc5a13df2c76b9ed94f832918920270
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-08 00:19:39 -0500

    Create PathOptions.java

---
+++ src/main/java/com/ethpalser/chess/space/PathOptions.java
@@ -0,0 +1,30 @@
+package com.ethpalser.chess.space;
+
+public record PathOptions(Type type, Reference start, Reference end) {
+
+    public PathOptions {
+        if (type == null) {
+            type = Type.CUSTOM;
+        }
+    }
+
+    public PathOptions(Type type) {
+        this(type, null, null);
+    }
+
+    public PathOptions(Reference reference) {
+        this(Type.CUSTOM, reference, null);
+    }
+
+    public PathOptions(Coordinate... path) {
+        this(Type.CUSTOM, new Reference(Reference.Location.PATH, Direction.AT, 1, path), null);
+    }
+
+
+    public enum Type {
+        HORIZONTAL,
+        VERTICAL,
+        DIAGONAL,
+        CUSTOM;
+    }
+}
