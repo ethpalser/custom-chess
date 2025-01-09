7cae749a4fb6018b5bf2fa30da458db1c597eea4
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-09 03:23:20 -0500

    Update PathStateConditional.java

--- src/main/java/com/ethpalser/chess/move/custom/condition/PathStateConditional.java
+++ src/main/java/com/ethpalser/chess/move/custom/condition/PathStateConditional.java
@@ -1,10 +1,14 @@
 package com.ethpalser.chess.move.custom.condition;
 
+import com.ethpalser.chess.board.Board;
 import com.ethpalser.chess.game.GameContext;
+import com.ethpalser.chess.piece.Piece;
 import com.ethpalser.chess.space.Coordinate;
 import com.ethpalser.chess.space.Path;
 import com.ethpalser.chess.space.PathFactory;
 import com.ethpalser.chess.space.PathOptions;
+import java.util.LinkedList;
+import java.util.List;
 
 public class PathStateConditional implements Conditional {
 
@@ -22,13 +26,29 @@ public class PathStateConditional implements Conditional {
     public boolean isExpected(GameContext.Record context, Coordinate appliedTo) {
         PathFactory factory = new PathFactory(context, appliedTo);
         Path path = factory.create(this.pathOptions);
+        List<Piece> piecesOfPath;
+        if (context == null || context.getBoard() == null) {
+            piecesOfPath = null;
+        } else {
+            piecesOfPath = new LinkedList<>();
+            for (Coordinate c : path) {
+                Piece p = context.getBoard().get(c);
+                if (p != null) {
+                    piecesOfPath.add(p);
+                }
+            }
+        }
         return switch (this.operator) {
-            case TRUE -> !path.isEmpty();
-            case FALSE -> path.isEmpty();
+            case TRUE -> piecesOfPath != null && !piecesOfPath.isEmpty();
+            case FALSE -> piecesOfPath != null && piecesOfPath.isEmpty();
             case EQUAL -> (this.expected == null && path.isEmpty())
                     || (this.expected instanceof Path && this.expected.equals(path));
             case NOT_EQUAL -> (this.expected == null && !path.isEmpty())
                     || (this.expected instanceof Path && !this.expected.equals(path));
         };
+    }
+
+    List<Piece> piecesOfPath(Path path, Board<Coordinate> board) {
+        return path.toList().stream().map(board::get).toList();
     }
 }
