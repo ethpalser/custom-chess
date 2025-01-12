40fcc2b99b17bf4551251270230f5132c62ddd21
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-12 16:32:12 -0500

    Update GamePrompt.java

--- src/main/java/com/ethpalser/chess/game/state/GamePrompt.java
+++ src/main/java/com/ethpalser/chess/game/state/GamePrompt.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public record GamePrompt(EventType eventType, Coordinate source, List<String> choices) {
     public GamePrompt {
-        if (eventType == null || source == null || choices == null || choices.isEmpty()) {
-            throw new IllegalArgumentException("One or more prompt values are null or empty");
+        if (eventType == null || source == null || choices == null) {
+            throw new IllegalArgumentException("One or more prompt values are null");
+        }
+        // Follow up is a special case, which should automatically treat an empty list as removing the source
+        if (!EventType.MOVE_FOLLOW_UP.equals(eventType) && choices.isEmpty()) {
+            throw new IllegalArgumentException("Provided prompt choices are empty");
         }
     }
 
