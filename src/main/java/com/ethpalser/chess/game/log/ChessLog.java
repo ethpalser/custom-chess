bd45c1109785ee654b9e6bf28d7432506342b6e9
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-12 16:30:17 -0500

    Update ChessLog.java

--- src/main/java/com/ethpalser/chess/game/log/ChessLog.java
+++ src/main/java/com/ethpalser/chess/game/log/ChessLog.java
@@ -3,6 +3,7 @@ package com.ethpalser.chess.game.log;
 import com.ethpalser.chess.game.event.GameEvent;
 import com.ethpalser.chess.game.event.GameEventProxy;
 import com.ethpalser.chess.move.notation.ChessNotation;
+import java.util.Objects;
 
 public class ChessLog extends GameLog<ChessLog.Entry> {
 
@@ -19,11 +20,24 @@ public class ChessLog extends GameLog<ChessLog.Entry> {
         }
     }
 
-    public record Entry(ChessNotation notation, GameEvent eventList) {
+    public record Entry(ChessNotation notation, GameEvent event) {
         public Entry {
-            if (notation == null || eventList == null) {
+            if (notation == null || event == null) {
                 throw new IllegalArgumentException("ChessLog Entry has at least one null argument");
             }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Entry entry = (Entry) o;
+            return Objects.equals(notation, entry.notation) && Objects.equals(event, entry.event);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(notation, event);
         }
     }
 
