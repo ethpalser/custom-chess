a151126ab2486652a859b381b37c02bf2222378d
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2025-01-12 16:35:22 -0500

    Update ReadyState.java to auto execute some prompts

--- src/main/java/com/ethpalser/chess/game/state/ReadyState.java
+++ src/main/java/com/ethpalser/chess/game/state/ReadyState.java
@@ -4,6 +4,13 @@ import com.ethpalser.chess.exception.IllegalActionException;
 import com.ethpalser.chess.game.GameContext;
 import com.ethpalser.chess.game.event.EventType;
 import com.ethpalser.chess.game.event.GameEvent;
+import com.ethpalser.chess.game.event.MoveFollowUpEvent;
+import com.ethpalser.chess.game.event.PromoteEvent;
+import com.ethpalser.chess.piece.Piece;
+import com.ethpalser.chess.piece.PieceType;
+import com.ethpalser.chess.space.Coordinate;
+import com.ethpalser.chess.space.Point;
+import java.util.List;
 
 public class ReadyState implements GameState {
 
@@ -22,10 +29,51 @@ public class ReadyState implements GameState {
             event.execute(this.context);
             GamePrompt prompt = this.context.getPrompt();
             if (prompt != null) {
-                return new AwaitState(this.context, prompt.eventType(), prompt.choices());
+                return this.handlePrompt(prompt);
+            } else {
+                return new ReadyState(this.context);
             }
         } else {
             throw new IllegalActionException("Event not supported by the current game state");
+        }
+    }
+
+    private GameState handlePrompt(GamePrompt prompt) {
+        Piece source = context.getBoard().get(prompt.source());
+        boolean isPawn = PieceType.PAWN.toCode().equals(source.getCode());
+
+        List<String> choices;
+        if (EventType.PROMOTE.equals(prompt.eventType()) && isPawn) {
+            choices = List.of("Q"); // Temporary. This is to automatically promote it to a queen
+        } else {
+            choices = prompt.choices();
+        }
+
+        if (choices.size() <= 1) {
+            this.context.clearPrompt();
+            String choice = prompt.choices().isEmpty() ? null : prompt.choices().get(0);
+            // Create the event to automatically perform
+            if (EventType.MOVE_FOLLOW_UP.equals(prompt.eventType())) {
+                Coordinate target;
+                if (choice == null) {
+                    target = null; // This should remove the piece
+                } else {
+                    target = new Point(choice);
+                }
+                new MoveFollowUpEvent(prompt.source(), target).execute(this.context);
+            } else if (EventType.PROMOTE.equals(prompt.eventType())) {
+                if (choice == null) {
+                    throw new IllegalArgumentException("Cannot promote with a null choice");
+                }
+                new PromoteEvent(prompt.source(), prompt.choices().get(0)).execute(this.context);
+            }
+            // Another prompt may have been raised
+            GamePrompt another = this.context.getPrompt();
+            if (another != null) {
+                return new AwaitState(this.context, another.eventType(), another.choices());
+            }
+        } else {
+            return new AwaitState(this.context, prompt.eventType(), prompt.choices());
         }
         return new ReadyState(this.context);
     }
